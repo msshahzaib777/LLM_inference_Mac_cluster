5a088d790ac0c728a31ff7f8884b9267401b5d04
Muhammad Shahzaib <ms.shahzaib777@gmail.com>
2025-04-14 11:42:37 +0300

    MLX distributed communication

---
+++ DeepSeekMLX.py
@@ -0,0 +1,17 @@
+from mlx_lm import load, generate
+import mlx.core as mx
+
+group = mx.distributed.init(backend="ring")
+
+
+model, tokenizer = load("mlx-community/DeepSeek-R1-Distill-Qwen-14B")
+model.to("ring")
+prompt="hello"
+
+if hasattr(tokenizer, "apply_chat_template") and tokenizer.chat_template is not None:
+    messages = [{"role": "user", "content": prompt}]
+    prompt = tokenizer.apply_chat_template(
+        messages, tokenize=False, add_generation_prompt=True
+    )
+
+response = generate(model, tokenizer, prompt=prompt, verbose=True)
