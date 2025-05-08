1d941424ec5c6e80f52bdf92816fb060f21d3f8b
Muhammad Shahzaib <ms.shahzaib777@gmail.com>
2025-05-08 23:11:58 +0300

    MPI with MLX type

--- network/mlx_mpi.py
+++ network/mlx_mpi.py
@@ -8,7 +8,7 @@ class MLXBackend(NetworkInterface):
         tensor_name = kwargs.get('tensor_name')
         log_debug(f"[Receiver] Receiving tensor '{tensor_name}' from rank {source_rank}")
         template_tensor = config.get_tensor_template(tensor_name)
-        return mx.distributed.recv_like(template_tensor, src=source_rank)
+        return mx.distributed.recv(template_tensor.shape, template_tensor.dtype, src=source_rank)
 
     def send_tensor(self, tensor, dest_rank=1, tag=0, **kwargs):
         log_debug(f"[Sender] Sending tensor to rank {dest_rank}")
