e6b1acf522223e0287f1f614afa65d65ba91b7ae
Muhammad Shahzaib <ms.shahzaib777@gmail.com>
2025-05-09 13:08:38 +0300

    MPI with MLX type

--- test.py
+++ test.py
@@ -14,17 +14,16 @@ elif rank == 1:
     _ = mx.distributed.recv((1,), mx.int32, src=0)
     print(f"[Rank {rank}] Received ready signal from rank 0", flush=True)
 
-if rank == 0:
-    tensor = mx.array([[1.0, 2.0], [3.0, 4.0]], dtype=mx.float32)
-    print(f"[Rank {rank}] Sending tensor:\n{tensor}", flush=True)
-    mx.distributed.send(tensor, dst=1)
-    print(f"[Rank {rank}] Send complete", flush=True)
-
-elif rank == 1:
+if rank == 1:
     shape = (2, 2)
     dtype = mx.float32
     print(f"[Rank {rank}] Waiting to receive tensor...", flush=True)
     received_tensor = mx.distributed.recv(shape, dtype, src=0)
     print(f"[Rank {rank}] Received tensor:\n{received_tensor}", flush=True)
+elif rank == 0:
+    tensor = mx.array([[1.0, 2.0], [3.0, 4.0]], dtype=mx.float32)
+    print(f"[Rank {rank}] Sending tensor:\n{tensor}", flush=True)
+    mx.distributed.send(tensor, dst=1)
+    print(f"[Rank {rank}] Send complete", flush=True)
 
 print(f"[Rank {rank}] Done.", flush=True)
