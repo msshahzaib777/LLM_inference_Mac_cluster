d20f9c03f324f74bc2831817e99ec689a8bd0bd1
Muhammad Shahzaib <ms.shahzaib777@gmail.com>
2025-05-09 13:45:25 +0300

    MPI with MLX type

--- test.py
+++ test.py
@@ -5,16 +5,15 @@ rank = group.rank()
 size = group.size()
 
 print(f"[Rank {rank}] Hello from rank {rank} of {size}", flush=True)
-
+tensor = mx.array([[1.0, 2.0], [3.0, 4.0]], dtype=mx.float32)
 if rank == 0:
-    tensor = mx.array([[1.0, 2.0], [3.0, 4.0]], dtype=mx.float32)
     print(f"[Rank {rank}] Sending tensor:\n{tensor}", flush=True)
     sent_tensor = mx.distributed.send(tensor, dst=1, group=group)
     print(f"[Rank {rank}] Send complete:\n{tensor}", flush=True)
 
 if rank == 1:
-    shape = (2, 2)
-    dtype = mx.float32
+    shape = tensor.shape
+    dtype = tensor.dtype
     tensor = mx.zeros(shape, dtype=dtype)
     print(f"[Rank {rank}] Waiting to receive tensor...", flush=True)
     received_tensor = mx.distributed.recv_like(tensor, src=0, group=group)
